4d1d3b904c8c4ae934141a882a074353ead556dc
Aw Khai Loong <sky99aw@gmail.com>
2021-10-10 12:26:57 +0800

    Add Amplify function for creating cognito accounts

--- src/amplify-cognito/AmplifyAuth.js
+++ src/amplify-cognito/AmplifyAuth.js
@@ -28,3 +28,27 @@ export async function getCurrentEmail() {
     const email = user.attributes.email;
     return email;
 }
+
+export async function createCognitoAccount(email) {
+    console.log("AmplifyAuth: createCognitoAccount");
+
+    const { user } = await Auth.signUp({
+        username: email,
+        password: generateRandomPassword(),
+        attributes: {
+            email: email
+        }
+    });
+}
+
+function generateRandomPassword() {
+    let length = 16,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        symbols = "!@#$%^&*()-_+=",
+        retVal = "";
+    for (let i = 0; i < length / 2; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * charset.length));
+        retVal += symbols.charAt(Math.floor(Math.random() * symbols.length))
+    }
+    return retVal;
+}
