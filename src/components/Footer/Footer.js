466e0c3bebfbf45e5a5403229ff739083e738714
yuhaolee97 <yuhaolee97@gmail.com>
2021-10-17 18:11:09 +0800

    edited footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,7 @@ class Footer extends Component {
         <Container fluid>
           <nav>
             <ul className="footer-menu">
-              <li>
+              {/* <li>
                 <a href="#pablo" onClick={(e) => e.preventDefault()}>
                   Home
                 </a>
@@ -44,12 +44,11 @@ class Footer extends Component {
                 <a href="#pablo" onClick={(e) => e.preventDefault()}>
                   Blog
                 </a>
-              </li>
+              </li> */}
             </ul>
             <p className="copyright text-center">
-              © {new Date().getFullYear()}{" "}
-              <a href="http://www.creative-tim.com">Creative Tim</a>, made with
-              love for a better web
+              COVID-19 F&B App - For all your needs 
+              
             </p>
           </nav>
         </Container>
