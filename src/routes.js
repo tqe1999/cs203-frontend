906a2608238eabad8dd0f3ddabd6f2df3907b99e
Qi En <tqe1999@gmail.com>
2021-11-14 17:03:20 +0800

    remove unused cross domain scripts, set cache control header

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,6 @@ import UserProfile from "views/UserProfile.js";
 import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
 import Icons from "views/Icons.js";
-import Maps from "views/Maps.js";
 import Notifications from "views/Notifications.js";
 import Upgrade from "views/Upgrade.js";
 import FootfallData from "views/FootfallData.js"
